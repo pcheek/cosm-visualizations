cc2218e945721cd97af69fa2c72463a922c6ab7e
Calum <calumb@gmail.com>
2013-06-03 10:20:28 -0400

    changes

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'qwgFCfpFeiKDzZjFlPvLrclFTPKSAKwvOGdhYmZmWFB0ND0g', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [119865,'124962!htemp'], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Calum\'s Feeds', // Replaces Xively logo in the header
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor		= 'dd0000', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 	
